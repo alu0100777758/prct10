de396330c13f9e5430f42547caa4e3a55733cad6
alu01007777581 <alu0100777758@edu.ull.es>
2014-11-20 20:37:08 +0000

    Interfaz v3

--- lib/simpleselect/interface.rb
+++ lib/simpleselect/interface.rb
@@ -7,16 +7,28 @@ class Interface<Examen
       for cuestion in @preguntas.headToTail do
          puts cuestion
          print "\e[96m Su respuesta: "
-         eleccion = gets.chomp.downcase[0]
+         loop do
+            @eleccion = gets.chomp.downcase[0]
+            if (@eleccion == nil)then
+               puts "Por favor introduzca una opcion valida"
+            else break 
+            end
+         end
          puts "\e[39m"
-         if ( compara_resp(@pos,eleccion) == true ) then
+         if ( compara_resp(@pos,@eleccion) == true ) then
             puts "\e[92m |-> Respuesta correcta! <-|\e[39m\n "
             @puntuacion += 1
          else
             puts "\e[91m |-> Respuesta incorrecta! <-|\e[39m\n "
          end
-         puts "\e[90mpulse enter para continuar\e[39m"
-         gets
+         loop do 
+            puts "\e[90mpulse enter para continuar (o i para mas informacion)\e[39m"
+            cuestion=gets.chomp.downcase[0]
+            if (cuestion == "i") then
+               puts "Actual:#{@pos}\nTotal:#{@n}\nAcertadas:#{@puntuacion}\nFallidas:#{@pos+1-@puntuacion}"
+            else break
+            end
+         end
          @pos += 1
       end
       print "ha obtenido usted una puntuacion de "
