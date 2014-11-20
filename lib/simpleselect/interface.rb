612d3bd146dca3455776bdff84f7bbe52a3c0442
alu01007777581 <alu0100777758@edu.ull.es>
2014-11-20 20:10:43 +0000

    Interfaz terminada primera version

--- lib/simpleselect/interface.rb
+++ lib/simpleselect/interface.rb
@@ -3,11 +3,28 @@ require "simpleselect/examen.rb"
 class Interface<Examen 
    def comenzar
       pos=0
+      puntuacion=0
       for cuestion in @preguntas.headToTail do
          puts cuestion
-         eleccion = gets.chomp
-         compara_resp(pos,eleccion)
+         print "\e[96m Su respuesta: "
+         eleccion = gets.chomp.downcase[0]
+         puts "\e[39m"
+         if ( compara_resp(pos,eleccion) == true ) then
+            puts "\e[92m |-> Respuesta correcta! <-|\e[39m\n "
+            puntuacion += 1
+         else
+            puts "\e[91m |-> Respuesta incorrecta! <-|\e[39m\n "
+         end
+         puts "\e[90mpulse enter para continuar\e[39m"
+         gets
          pos += 1
       end
+      print "ha obtenido usted una puntuacion de "
+      if (puntuacion < (pos/2) ) then 
+         print "\e[91m #{puntuacion}"
+      else
+         print "\e[96m #{puntuacion}"
+      end
+      print"\e[39m Sobre "+pos.to_s+"\n"
    end
 end
